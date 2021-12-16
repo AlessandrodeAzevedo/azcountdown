f00fc80519d7eee846b39019b5c55f9d9cff9784
Alessandro de Azevedo <alessandrodeazevedo@gmail.com>
2021-12-15 22:08:19 -0300

    Initial project

---
+++ az-countdown.js
@@ -0,0 +1,157 @@
+/*
+    Name   : az-countdown
+    version: v0.1
+    author : @alessandrodeazevedo
+    license: MIT
+*/
+class AzCountdown
+{
+    elementID = 'AzCountdown';
+    uri;
+    key;
+    localstorageKey;
+    uniqId = this.guidGenerator();
+    date = new Date(Date.now() + ( 3600 * 1000 * 168));
+    constructor ( obj = null) {
+        if (obj) {
+            if (typeof obj.elementID !== 'undefined') {
+                this.elementID = obj.elementID;
+            }
+            if (typeof obj.date !== 'undefined') {
+                this.date = new Date(obj.date);
+            }
+            if (typeof obj.key !== 'undefined') {
+                this.key = obj.key;
+            }
+            if (typeof obj.uri !== 'undefined') {
+                this.uri = obj.uri;
+            }
+            if (typeof obj.localstorageKey !== 'undefined') {
+                this.localstorageKey = obj.localstorageKey;
+            }
+        }
+        //this.getDateFromLocalStorage()
+        let recipient = document.getElementById(this.elementID);
+        let azCountdown = document.createElement('div');
+        azCountdown.style.display = 'flex';
+        azCountdown.style.justifyContent = 'space-around';
+        azCountdown.style.padding = '20px 10px';
+        azCountdown.id = `azCountdown_${this.uniqId}`;
+        recipient.appendChild(azCountdown);
+        this.azCreateElement('DIAS', `azDays`);
+        this.azCreateElement('HORAS', `azHours`);
+        this.azCreateElement('MINUTOS', `azMinutes`);
+        this.azCreateElement('SEGUNDOS', `azSeconds`, true);
+        if (this.localstorageKey) {
+            this.azCountdownSetCounter(
+                this.getDateFromLocalStorage(), // or new Date('2021-08-06 00:00:00')
+                `#azDays_${this.uniqId}`, // element for days or false
+                `#azHours_${this.uniqId}`, // element for hours or false
+                `#azMinutes_${this.uniqId}`, // element for minutes or false
+                `#azSeconds_${this.uniqId}`  // element for seconds or false
+            );
+        } else if (!this.uri) {
+            this.azCountdownSetCounter(
+                this.date, // or new Date('2021-08-06 00:00:00')
+                `#azDays_${this.uniqId}`, // element for days or false
+                `#azHours_${this.uniqId}`, // element for hours or false
+                `#azMinutes_${this.uniqId}`, // element for minutes or false
+                `#azSeconds_${this.uniqId}`  // element for seconds or false
+            );
+        } else {
+            this.file_get_contents(this.uri);
+        }
+    }
+    getDateFromLocalStorage() {
+        /* let date = localStorage.getItem(this.localstorageKey);
+        if (!date) {
+            localStorage.setItem(this.localstorageKey) = this.date;
+        } else {
+            date = new Date(date)
+        } */
+        return false; 
+    }
+    guidGenerator() {
+        var S4 = function() {
+           return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+        };
+        return (S4()+S4());
+    }
+    azCreateElement(label, id,isRed = false){
+        let azCountdown = document.getElementById(`azCountdown_${this.uniqId}`);
+        let azCountdownWrapper = document.createElement('div');
+        azCountdownWrapper.style.backgroundColor = 'white';
+        azCountdownWrapper.style.textAlign = 'center';
+        azCountdownWrapper.style.minWidth = '55px';
+        azCountdownWrapper.style.padding = '15px 10px';
+        azCountdownWrapper.style.borderRadius = '5px';
+        let azCountdownNumber = document.createElement('div');
+        azCountdownNumber.style.fontFamily = 'Oswald, sans-serif';
+        azCountdownNumber.style.fontSize = '45px';
+        azCountdownNumber.innerHTML = 0;
+        azCountdownNumber.id = id + '_' + this.uniqId;
+        
+        let azCountdownLabel = document.createElement('div');
+        azCountdownLabel.style.color = 'rgb(155, 155, 155)';
+        azCountdownLabel.style.fontSize = '10px';
+        azCountdownLabel.style.fontFamily = 'Arial, "Helvetica Neue", Helvetica, sans-serif';
+        
+        if (isRed){
+            azCountdownWrapper.style.backgroundColor = 'rgba(208,2,27,1)';
+            azCountdownNumber.style.color = 'white';
+            azCountdownLabel.style.color = 'white';
+        }
+        azCountdownLabel.innerHTML = label;
+        azCountdownWrapper.appendChild(azCountdownNumber);
+        azCountdownWrapper.appendChild(azCountdownLabel);
+        azCountdown.appendChild(azCountdownWrapper);
+    }
+    azCountdownSetCounter (date, id_days, id_hours, id_minutes, id_seconds) {
+        var x = setInterval(function() {
+            var now = new Date().getTime();
+            var distance = date - now;
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            days = days >= 0 ? days : 0;
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            hours = hours >= 0 ? hours : 0;
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            minutes = minutes >= 0 ? minutes : 0;
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            seconds = seconds >= 0 ? seconds : 0;
+            if (id_days) {
+                document.querySelector(id_days).innerHTML = days;
+            }
+            if (id_hours) {
+                document.querySelector(id_hours).innerHTML = hours;
+            }
+            if (id_minutes) {
+                document.querySelector(id_minutes).innerHTML = minutes;
+            }
+            if (id_seconds) {
+                document.querySelector(id_seconds).innerHTML = seconds;
+            }
+        },1000);
+    }
+    async file_get_contents(uri) {
+        let res = await fetch(uri),
+        ret = await res.text();
+        var parser = new DOMParser();
+        // Parse the text
+        var doc = parser.parseFromString(ret, "text/html");
+        var educbrCountdown = doc.getElementsByTagName('az-countdown');
+        for (let i = 0; educbrCountdown.length > i; i++) {
+            let data = JSON.parse(educbrCountdown[i].innerHTML);
+            if (data.key == this.key){
+                this.azCountdownSetCounter(
+                    new Date(data.date), // or new Date('2021-08-06 00:00:00')
+                    `#azDays_${this.uniqId}`, // element for days or false
+                    `#azHours_${this.uniqId}`, // element for hours or false
+                    `#azMinutes_${this.uniqId}`, // element for minutes or false
+                    `#azSeconds_${this.uniqId}`  // element for seconds or false
+                );
+            }
+        }
+        //return callback ? callback(ret) : ret; // a Promise() actually.
+        //return callback ? callback(ret) : ret; // a Promise() actually.
+    }
+}
